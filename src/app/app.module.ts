c5b80b7464eef94a24311d1411013df55a0da9a8
ginamdar <guru.inamdar@gmail.com>
2020-05-13 15:30:19 -0400

    setting up @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { ProductComponent } from "./products/components/product/product.component";
-import { ProductAddComponent } from "./products/components/product-add/product-add.component";
-import { ProductEditComponent } from "./products/components/product-edit/product-edit.component";
-import { ProductListComponent } from "./products/components/product-list/product-list.component";
 import { HomeComponent } from "./pages/home/home.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 import { ProductsModule } from "./products/products.module";
+import {StoreModule} from '@ngrx/store';
+import {metaReducers, reducers} from './reducers';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -20,7 +18,14 @@ import { ProductsModule } from "./products/products.module";
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    ProductsModule
+    ProductsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
